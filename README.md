3ac736f92012152a6d8114eda83aa71a0f761028
sshmatrix <cryptosauron@protonmail.com>
2022-11-29 14:23:00 +0100

    test 0

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ https://twitter.com/nicksdjohnson/status/1597379086153764864
 
 ## Test Fun
 
-- Install `python3` [Source](https://www.python.org/downloads/)
+- Install `python3` [[Source](https://www.python.org/downloads/)]
 
 - Install dependencies: `pip install -r dependencies.log`
 
--- test.py
+++ test.py
@@ -6,11 +6,23 @@ import binascii
 import mnemonic
 import bip32utils
 
+enlib = 'english.log'
 mobj = mnemonic.Mnemonic("english")
+enDict = open(enlib, "r").read().split('\n')
+# load input and dict
+arr = input('Enter space-separated words: ')
+sus = list(map(str, arr.split(' ')))
+approved = []
+for item in sus:
+    if item in enDict:
+        approved.append(item)
+
+print('Valid BIP39 words: ')
+print(*approved, sep=' ')
 
 def bip39(mnemonic_words):
     seed = mobj.to_seed(mnemonic_words)
-
+    entropy = mobj.to_entropy(mnemonic_words)
     bip32_root_key_obj = bip32utils.BIP32Key.fromEntropy(seed)
     bip32_child_key_obj = bip32_root_key_obj.ChildKey(
         44 + bip32utils.BIP32_HARDEN
@@ -22,6 +34,7 @@ def bip39(mnemonic_words):
 
     return {
         'mnemonic_words': mnemonic_words,
+        'entropy': entropy,
         'addr': bip32_child_key_obj.Address(),
         'publickey': binascii.hexlify(bip32_child_key_obj.PublicKey()).decode(),
         'privatekey': bip32_child_key_obj.WalletImportFormat(),
@@ -29,6 +42,10 @@ def bip39(mnemonic_words):
     }
 
 if __name__ == '__main__':
-    mobj = mnemonic.Mnemonic("english")
-    mnemonic_words = mobj.generate(strength=128)
-    pprint.pprint(bip39(mnemonic_words))
+    if len(approved) >= 12:
+        print('CONGRATS! Number of words >= 12')
+    else:
+        print('ERROR: Number of words < 12')
+
+    #words = mobj.generate(strength=128)
+    #pprint.pprint(bip39(words))
