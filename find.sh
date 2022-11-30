a666855305eb627982044f54eda418349ab1e3f4
sshmatrix <cryptosauron@protonmail.com>
2022-11-30 15:02:08 +0100

    sync

--- find.sh
+++ find.sh
@@ -3,7 +3,7 @@ START_T=$(date +%s)
 source .env
 HERE=/work/$USER/midjourney
 ${HERE}/dist/test $ID 1000000
-touch ${HERE}/${ID}.flag
+touch ${HERE}/flags/${ID}.flag
 cp ./test.key ${HERE}/ > /dev/null 2>&1
 END_T=$(date +%s)
 DIFF_T=$(( $END_T - $START_T ))
